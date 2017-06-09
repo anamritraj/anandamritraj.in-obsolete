7c769f1c78819532b0d2441e75c68aa05e6dbe21
anamritraj <anandamritraj904@gmail.com>
2017-06-09 22:03:11 +0530

    Add css minimization.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,9 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
 gulp.task('browser-sync', function() {
     browserSync.init({
         proxy: "http://localhost/anandamritraj/"
@@ -11,11 +14,18 @@ gulp.task('browser-sync', function() {
 gulp.task('styles', function() {
     gulp.src('assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css/'));
+        .pipe(gulp.dest('./assets/sass/'));
 });
 
 gulp.task('css', function(){
 	return gulp.watch("assets/**/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('css-min', function () {
+    gulp.src('assets/sass/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('php', function(){
@@ -28,7 +38,7 @@ gulp.task('js', function(){
 
 //Watch task
 gulp.task('scss', function() {
-    return gulp.watch('assets/scss/**/*.scss', ['styles']);
+    return gulp.watch('assets/scss/**/*.scss', ['styles', 'css-min']);
 });
 
 gulp.task('default', ['browser-sync','scss', 'php', 'js', 'css']);
